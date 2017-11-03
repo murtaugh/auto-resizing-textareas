2ff710f5bd21525091a2fdad5ad74e7e4d4531d6
Tim Murtaugh <tim@o2b.net>
2017-11-03 10:30:11 -0400

    minor upgrades

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Simple and graceful auto-resizing textareas. Plays nice with non-resizing textareas and supports as many as you care to have.
 
-I got almost all of the JS for this from @jlong: https://gist.github.com/jlong/2127634
+I got the basics for this from @jlong: https://gist.github.com/jlong/2127634
--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,9 +3,9 @@
 $(document).ready(function (){
 
 	$('textarea[data-autoresize]').on('input propertychange focus', function() {
-		
+
 		autoResize($(this));
-		
+
 	});
 
 });
@@ -17,12 +17,12 @@ $(document).ready(function (){
 
 /*
 	Functions for auto-resizing textareas.
-	Based almost entirely on John Long's excellent autogrow plugin (except I un-plugin-ifyed it):
+	Based on John Long's excellent autogrow plugin:
 	https://gist.github.com/jlong/2127634
-	
+
 	For a more consistent user experience you should apply resize: none to auto-resizing textareas in your CSS.
-	The plugin needs to apply it on its own, so it's better that it not be there at all. (That said, you probably
-	want to apply that style only when JS is avaialble.)
+	The plugin needs to apply it on its own, so it's better that it not be there at all.
+	(That said, you probably want to apply that style only when JS is available.)
 */
 
 var properties = ['-webkit-appearance','-moz-appearance','-o-appearance','appearance','font-family','font-size','font-weight','font-style','border','border-top','border-right','border-bottom','border-left','box-sizing','padding','padding-top','padding-right','padding-bottom','padding-left','min-height','max-height','line-height'],	escaper = $('<span />');
@@ -32,10 +32,10 @@ function escape(string) {
 };
 
 function autoResize(which) {
-	
+
 	if (!which.data('autogrow-applied')) {
-	
-		var textarea = which,	 initialHeight = textarea.innerHeight(), expander = $('<div />'), timer = null;
+
+		var textarea = which, initialHeight = textarea.innerHeight(), expander = $('<div />'), timer = null;
 
 		// Setup expander
 		expander.css({'position': 'absolute', 'visibility': 'hidden', 'bottom': '110%'})
@@ -53,7 +53,7 @@ function autoResize(which) {
 						expander.html(value);
 						expander.css('width', textarea.innerWidth() + 2 + 'px');
 						textarea.css('height', Math.max(expander.innerHeight(), initialHeight) + 2 + 'px');
-				}, 100); // throttle by 100ms 
+				}, 100); // throttle by 100ms
 		}
 
 		// Bind sizer to IE 9+'s input event and Safari's propertychange event
@@ -64,7 +64,7 @@ function autoResize(which) {
 
 		// Record autogrow applied
 		textarea.data('autogrow-applied', true);
-		
+
 	};
-	
+
 };
